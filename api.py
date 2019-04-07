bf71770cf479127a8d1f9d3296c4351c3989f41e
Harsh mishra <41248662+harsh199910@users.noreply.github.com>
2019-04-07 12:47:45 +0530

    Add files via upload

---
+++ api.py
@@ -0,0 +1,41 @@
+from flask import Flask, request, jsonify
+from sklearn.externals import joblib
+import traceback
+import pandas as pd
+import numpy as np
+
+# Your API definition
+app = Flask(__name__)
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    if classifier:
+        try:
+            json_ = request.json
+            print(json_)
+            query = pd.get_dummies(pd.DataFrame(json_))
+            query = query.reindex(columns=model_columns, fill_value=0)
+
+            prediction = list(lr.predict(query))
+
+            return jsonify({'prediction': str(prediction)})
+
+        except:
+
+            return jsonify({'trace': traceback.format_exc()})
+    else:
+        print ('Train the model first')
+        return ('No model here to use')
+
+if __name__ == '__main__':
+    try:
+        port = int(sys.argv[1]) # This is for a command-line input
+    except:
+        port = 12345 # If you don't provide any port the port will be set to 12345
+
+    classifier = joblib.load("model.pkl") # Load "model.pkl"
+    print ('Model loaded')
+    
+    print ('Model columns loaded')
+
+    app.run(port=port, debug=True)
